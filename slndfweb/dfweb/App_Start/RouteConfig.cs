7df3ec2848a417d1d628f1a628076384651418a7
Tzu Chieh Chang <lookout0709@gmail.com>
2020-09-07 01:21:37 +0800

    更新route

--- slndfweb/dfweb/App_Start/RouteConfig.cs
+++ slndfweb/dfweb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace dfweb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "dfmembers", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
